887855fa1b09caa8a6c4ec6e297c248a43c7c8c5
tsheeran1 <tom@beast.lan>
2019-05-02 21:52:44 -0400

    commit

--- store.go
+++ store.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 type Event struct {
@@ -13,14 +18,64 @@ type Event struct {
 	Income int `json:"income"`
 }
 
+type Record struct {
+	Userid string
+	Age    int
+	Height int
+	Income int
+}
+
+type Output interface{}
+
 func handler(ctx context.Context, e Event) (string, error) {
 
-	fmt.Println("Event:", e)
-	fmt.Println("Age: ", e.Age)
-	eage := e.Age
-	return fmt.Sprintf("%v", eage*2), nil
+	// return fmt.Sprintf("%v", eage*2), nil
+
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println("Event 1 : ", e)
+	r := Record{
+		Userid: fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]),
+		Age:    e.Age,
+		Height: e.Height,
+		Income: e.Income,
+	}
+
+	config := &aws.Config{
+		Region: aws.String("us-east-2"),
+	}
+
+	sess := session.Must(session.NewSession(config))
+
+	dbc := dynamodb.New(sess)
+
+	av, err := dynamodbattribute.MarshalMap(r)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to marshal object, %v", err))
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("compare-yourself"),
+	}
+
+	fmt.Println("input", input)
+
+	data, err := dbc.PutItem(input)
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	fmt.Println("data", data)
+
+	return fmt.Sprintf("%v", data), nil
 }
 
 func main() {
+
 	lambda.Start(handler)
+
 }
